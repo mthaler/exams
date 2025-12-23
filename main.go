69756c0715662419895dbb9058b20dd4c0ebe850
Michael Thaler <michael.thaler2@gmail.com>
2025-12-23 07:07:50 +0100

    Updated main.go.

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"encoding/csv"
 	"log"
 	"os"
+	"strconv"
+
+	"gonum.org/v1/plot/plotter"
 )
 
 func main() {
@@ -17,6 +20,20 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	// by slicing the records we skip the header
+	records = records[1:]
+	columnsValues := map[int]plotter.Values{}
 	for i, record := range records {
+		for c := 2; c < exams.FieldsPerRecord; c++ {
+			if _, found := columnsValues[c]; !found {
+				columnsValues[c] = make(plotter.Values, len(records))
+			}
+			// we parse each close value and add it to our set
+			floatVal, err := strconv.ParseFloat(record[c], 64)
+			if err != nil {
+				log.Fatal(err)
+			}
+			columnsValues[c][i] = floatVal
+		}
 	}
 }
